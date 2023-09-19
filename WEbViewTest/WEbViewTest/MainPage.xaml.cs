e6ced6a86391a0509089f8d0406ededf620b96d1
jorgemoore <jorge@idosde.com>
2023-09-19 18:53:06 -0500

    Inicial

--- WEbViewTest/WEbViewTest/MainPage.xaml.cs
+++ WEbViewTest/WEbViewTest/MainPage.xaml.cs
@@ -14,5 +14,46 @@ namespace WEbViewTest
         {
             InitializeComponent();
         }
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await prgloading.ProgressTo(0.9, 900, Easing.SpringIn);
+        }
+        private void Openurl(object sender, EventArgs e)
+        {
+            Browser.Source = Entryurl.Text;
+            prgloading.IsVisible = true;
+
+        }
+        void Handle_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            prgloading.IsVisible = false;
+            switch (e.Result)
+            {
+                case WebNavigationResult.Cancel:
+                    // TODO - do stuff here
+                    break;
+                case WebNavigationResult.Failure:
+                    var htmlSource = new HtmlWebViewSource();
+                    htmlSource.Html = @"<html><body><p>Intente usar la url completa incluyendo https://</p></body></html>";
+                   
+
+                    Browser.Source = htmlSource;
+                    break;
+                case WebNavigationResult.Success:
+                    // TODO - do stuff here
+                    break;
+                case WebNavigationResult.Timeout:
+                    // TODO - do stuff here
+                    break;
+                default:
+                    // TODO - do stuff here
+                    break;
+            }
+        }
+        void Handle_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            prgloading.IsVisible = true;
+        }
     }
 }
